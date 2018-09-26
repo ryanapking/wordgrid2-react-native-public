9de46f1b4f85123714b63ee6778de541c8ea11f7
Ryan Apking <ryan.apking@gmail.com>
2018-09-25 20:07:16 -0700

    Moving join and create remote games out of the gameData duck and into the games component.

--- routes/Games.js
+++ routes/Games.js
@@ -3,14 +3,18 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-native';
 import { List, ListItem, Button } from 'native-base';
+import firebase from "react-native-firebase";
 
-import { joinRemoteGame } from '../ducks/gameData';
+import {joinRemoteGame, remoteSaveGame} from '../ducks/gameData';
+import {generateGame} from "../utilities";
 
 class Games extends Component {
   constructor() {
     super();
-    this.createGame = this.createGame.bind(this);
+    this.joinRemoteGame = this.joinRemoteGame.bind(this);
+    this.createRemoteGame = this.createRemoteGame.bind(this);
   }
+
   render() {
     // console.log('game data:', this.props.gameData);
 
@@ -45,15 +49,122 @@ class Games extends Component {
           )}
         </List>
 
-        <Button full info onPress={() => this.createGame()}>
-          <Text>Create Game</Text>
+        <Button full info onPress={() => this.joinRemoteGame()}>
+          <Text>New Game</Text>
         </Button>
       </View>
     );
   }
 
-  createGame() {
-    this.props.joinRemoteGame(this.props.uid);
+  joinRemoteGame() {
+
+    // if the join game fails, a new game is created instead
+
+    const userID = this.props.uid;
+    const gamesCollectionRef = firebase.firestore().collection('games');
+    const userDocRef = firebase.firestore().collection('users').doc(userID);
+
+    // query to find a game waiting for a partner
+    return gamesCollectionRef
+      .where( "t", "==", "p2" )
+      .where( "p2", "==", null )
+      .limit(1)
+      .get()
+      .then( (games) => {
+
+        console.log('games', games);
+
+        // game found
+        if (games.docs.length > 0) {
+
+          const gameDoc = games.docs[0];
+          const joinGameID = { id: gameDoc.id, name: gameDoc.id };
+          const joinGameDocRef = gamesCollectionRef.doc(gameDoc.id);
+
+          // inequality queries don't exist in firestore
+          // if we happened to grab our own game, then we are out of luck
+          if (gameDoc.data().p1 === userID) {
+            console.log('we grabbed our own game! creating a new one...');
+            this.createRemoteGame();
+            return;
+          }
+
+          // try to claim the game in firebase
+          firebase.firestore().runTransaction( (transaction) => {
+            return transaction.get(userDocRef).then( (userDoc) => {
+
+              if (!userDoc.exists) {
+                transaction.set(userDocRef, {
+                  games: [joinGameID]
+                });
+              } else {
+                const currentUserGames = userDoc.data().games ? userDoc.data().games : [];
+                transaction.update(userDocRef, {
+                  games: [ ...currentUserGames, joinGameID ]
+                });
+              }
+              transaction.update(joinGameDocRef, {
+                t: userID,
+                p2: userID
+              });
+
+            }).then( () => {
+              console.log('join game success');
+            }).catch( (err) => {
+              console.log('failed to join game. creating a new one...');
+              console.log('just in case, printing error:', err);
+              this.createRemoteGame();
+            });
+          });
+
+        } else {
+          console.log('found no game to join. creating a new one...')
+          this.createRemoteGame();
+        }
+      })
+      .catch( (err) => {
+        // query failed
+        // (failure is not the same as no results. probably connection issues)
+        console.log('err', err);
+      });
+
+  }
+
+  createRemoteGame() {
+
+    const userID = this.props.uid;
+    const newGameData = generateGame(userID);
+    const userDocRef = firebase.firestore().collection('users').doc(userID);
+    const newGameDocRef = firebase.firestore().collection('games').doc();
+    const newGameID = { id: newGameDocRef.id, name: newGameDocRef.id };
+
+    return firebase.firestore().runTransaction( (transaction) => {
+
+      return transaction.get(userDocRef).then( (userDoc) => {
+
+        if (!userDoc.exists) {
+          transaction.set(userDocRef, {
+            games: [newGameID]
+          });
+        } else {
+          const currentUserGames = userDoc.data().games ? userDoc.data().games : [];
+          transaction.update(userDocRef, {
+            games: [ ...currentUserGames, newGameID ]
+          });
+        }
+
+        transaction.set(newGameDocRef, newGameData );
+
+        console.log('gameDocRef:', newGameDocRef);
+
+      });
+
+    }).then( () => {
+      console.log('sweet game save successsssss');
+    }).catch( (err) => {
+      console.log(err);
+    });
+
   }
 }
 
