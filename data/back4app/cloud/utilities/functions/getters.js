7113977d9d1743be56575a6931578351e4e15046
Ryan Apking <ryan.apking@gmail.com>
2019-01-27 10:33:40 -0800

    Remove the getScoreBoard utility function that is not included in the main game data conversion function.

--- data/back4app/cloud/utilities/functions/getters.js
+++ data/back4app/cloud/utilities/functions/getters.js
@@ -1,57 +1,5 @@
-const config = require('../config');
-const { settings } = config;
-
 const dataConversions = require('./dataConversions');
 const { boardStringToArray, pieceStringToArray, placementRefStringToArray } = dataConversions;
-
-// return a dumb object of scores as if they are baseball innings
-function getScoreBoard(history, p1, p2) {
-
-  return [];
-
-  const emptyScoreObject = { p1: null, p2: null };
-
-  const scores = history.reduce( (scores, move) => {
-
-    let previousMove = scores[scores.length - 1];
-
-    const lastScoreIndex = scores.length - 1;
-    let player = null;
-
-    if (!move.p) {
-      return scores;
-    } else if (move.p === p1) {
-      player = "p1";
-    } else if (move.p === p2) {
-      player = "p2";
-    } else {
-      console.log('something is busted');
-      return scores;
-    }
-
-    if (previousMove[player]) {
-      return [
-        ...scores,
-        {...emptyScoreObject, [player]: move.wv }
-      ];
-    } else {
-      scores[lastScoreIndex] = {...previousMove, [player]: move.wv};
-      return scores;
-    }
-
-  }, [emptyScoreObject]);
-
-  // if we are short, fill out the difference with empty inning objects
-  // this allows the scoreboard to go into extra innings and also display the remaining moves
-  const remainingTurnCount = settings.naturalTurns - scores.length;
-  if (remainingTurnCount > 0) {
-    let diffMakerUpper = new Array(remainingTurnCount).fill(emptyScoreObject);
-    return [...scores, ...diffMakerUpper];
-  } else {
-    return scores;
-  }
-
-}
 
 function getAnimationData(game) {
   if (game.history.length < 1) return false;
