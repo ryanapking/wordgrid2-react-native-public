a1b940cfffe92b3c75b19be2aa9bd15c2db0bc34
Ryan Apking <ryan.apking@gmail.com>
2019-02-01 06:11:49 -0800

    Add cloud function to generate a new challenge.

--- data/back4app/cloud/main.js
+++ data/back4app/cloud/main.js
@@ -3,6 +3,26 @@ const generators = require ('./utilities/functions/generators.js');
 const dataConversions = require('./utilities/functions/dataConversions');
 const validators = require('./utilities/functions/checks');
 const applyMoves = require('./utilities/functions/applyMoves');
+let moment = require('moment');
+
+Parse.Cloud.job("createNewChallenge", async function(request) {
+  const challenge = generators.generateChallenge();
+  let now = moment();
+
+  const ChallengesObject = Parse.Object.extend("Challenges");
+  let challengeObject = await new ChallengesObject()
+    .set("startDate", now.toDate())
+    .set("endDate", now.add(1, "day").toDate())
+    .set("startingBoard", challenge.startingBoard)
+    .set("startingPieces", challenge.startingPieces)
+    .set("pieceBank", challenge.pieceBank)
+    .save(null, { useMasterKey: true})
+    .catch( (err) => {
+      throw new Error(err);
+    });
+
+  return challengeObject;
+});
 
 Parse.Cloud.define("saveMove", async function(request) {
   if (!request.user) throw new Error('invalid user');
