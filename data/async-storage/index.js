a4519703aee0d6e02693f281b9ed3c2336e8873e
Ryan Apking <ryan.apking@gmail.com>
2019-02-02 14:56:35 -0800

    Update the async storage functions and add some new ones.

--- data/async-storage/index.js
+++ data/async-storage/index.js
@@ -1,86 +1,118 @@
 import { AsyncStorage } from 'react-native';
 
-import { store } from '../../App';
-import { setAttemptsHistory } from '../redux/challengeData';
-
 const prefix = "userChallenge:";
 
 // structure:
 const userTemplate = {
-  challengeIDs: [], // array of challenge IDs (timestamp generated at daily challenge creation)
-  challengesByID: {}, // object containing challenge starting point
-  attemptsByID: {}, // user challenge attempts
+  challengesByDate: {}, // challenge starting points
+  attemptsByDate: {}, // user challenge attempts
 };
 
-export function storeChallengeAttempt(uid, challengeData, challengeAttempt) {
-  return new Promise( (resolve) => {
-
-    const strippedChallengeAttempt = {
-      history: challengeAttempt.history,
-      score: challengeAttempt.score,
-    };
-
-    const challengeID = String(challengeAttempt.id);
-
-    AsyncStorage.getItem(prefix + uid, (error, result) => {
-      // parse the results if they exist, use above template if they don't
-      let userChallengeData = result ? JSON.parse(result) : {...userTemplate};
-      let { challengeIDs, challengesByID, attemptsByID } = userChallengeData;
-
-      // create the keys if needed
-      if (!challengeIDs.includes(challengeID)) challengeIDs.push(challengeID);
-      if (!(challengeID in challengesByID)) challengesByID[challengeID] = challengeData;
-      if (!(challengeID in attemptsByID)) attemptsByID[challengeID] = [];
-
-      // add the challenge attempt
-      attemptsByID[challengeID].push(strippedChallengeAttempt);
-
-      let setValue = {
-        challengeIDs,
-        challengesByID,
-        attemptsByID,
-      };
-      setValue = purgeOldAttempts({...setValue});
-
-      resolve(setValue);
-      AsyncStorage.setItem(prefix + uid, JSON.stringify(setValue));
+export async function storeChallengeAttemptByDate(uid, challengeAttempt, date) {
+  let userStorageObject = await getUserStorageObject(uid)
+    .catch((err) => {
+      throw new Error(err);
     });
 
-  });
+  if (userStorageObject.attemptsByDate.hasOwnProperty(date)) {
+    userStorageObject.attemptsByDate[date].push(challengeAttempt);
+  } else {
+    userStorageObject.attemptsByDate[date] = [challengeAttempt];
+  }
+
+  return await saveUserStorageObject(uid, userStorageObject)
+    .catch( (err) => {
+      throw new Error(err);
+    });
 }
 
-function purgeOldAttempts(userChallengeData) {
-  let { challengeIDs, challengesByID, attemptsByID } = userChallengeData;
+export async function storeChallengeByDate(uid, challenge, date) {
+  let userStorageObject = await getUserStorageObject(uid)
+    .catch((err) => {
+      throw new Error(err)
+    });
 
-  challengeIDs = challengeIDs.sort().slice(-10);
+  userStorageObject.challengesByDate[date] = challenge;
 
-  Object.keys(challengesByID).forEach( (key) => {
-    if (!challengeIDs.includes(key)) delete challengesByID[key];
-  });
-
-  Object.keys(attemptsByID).forEach( (key) => {
-    if (!challengeIDs.includes(key)) delete attemptsByID[key];
-  });
-
-  return {
-    challengeIDs,
-    challengesByID,
-    attemptsByID,
-  };
+  return await saveUserStorageObject(uid, userStorageObject)
+    .catch( (err) => {
+      throw new Error(err);
+    });
 }
 
-export function retrieveChallengeAttempts(uid) {
-  return new Promise( (resolve, reject) => {
-    AsyncStorage.getItem(prefix + uid)
-      .then( (result) => {
-        resolve(JSON.parse(result));
-      })
-      .catch( (err) => {
-        reject(err);
-      });
-  });
+export async function getChallengeByDate(uid, date) {
+  let userStorageObject = await getUserStorageObject(uid)
+    .catch( (err) => {
+      throw new Error(err);
+    });
+
+  if (userStorageObject.challengesByDate.hasOwnProperty(date)) {
+    return userStorageObject.challengesByDate[date];
+  } else {
+    throw new Error("no challenge found for that date");
+  }
 }
 
-export function clearUserData(uid) {
-  AsyncStorage.removeItem(prefix + uid);
+export async function getChallengeAttemptsByDate(uid, date) {
+  let userStorageObject = await getUserStorageObject(uid)
+    .catch( (err) => {
+      throw new Error(err);
+    });
+
+  if (userStorageObject.attemptsByDate.hasOwnProperty(date)) {
+    return userStorageObject.attemptsByDate[date];
+  } else {
+    return [];
+  }
+}
+
+export async function deleteChallengeDataByDate(uid, date) {
+  let userStorageObject = await getUserStorageObject(uid)
+    .catch( (err) => {
+      throw new Error(err);
+    });
+
+  delete userStorageObject.challengesByDate[date];
+  delete userStorageObject.attemptsByDate[date];
+
+  return await saveUserStorageObject(uid, userStorageObject)
+    .catch( (err) => {
+      throw new Error(err);
+    })
+}
+
+export async function clearUserChallengeData(uid) {
+  return await AsyncStorage
+    .removeItem(prefix + uid)
+    .then( () => {
+      return "success"
+    })
+    .catch( (err) => {
+      throw new Error(err);
+    })
+}
+
+async function getUserStorageObject(uid) {
+  let userStorageObject = await AsyncStorage
+    .getItem(prefix + uid)
+    .catch((err) => {
+      throw new Error(err)
+    });
+
+  // create a new user object or parse existing
+  if (!userStorageObject) {
+    return {...userTemplate};
+  } else {
+    return JSON.parse(userStorageObject);
+  }
+}
+
+async function saveUserStorageObject(uid, userStorageObject) {
+  await AsyncStorage
+    .setItem(prefix + uid, JSON.stringify(userStorageObject))
+    .catch( (err) => {
+      throw new Error(err);
+    });
+
+  return userStorageObject;
 }
