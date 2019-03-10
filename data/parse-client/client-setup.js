64b6e1b81e6b0e70d2cef268d8d23ff67afc20fe
Ryan Apking <ryan.apking@gmail.com>
2019-03-10 15:17:58 -0700

    Updating local version of parse livequery url to match our docker-compose setup.

--- data/parse-client/client-setup.js
+++ data/parse-client/client-setup.js
@@ -6,7 +6,7 @@ Parse.initialize("RyanAppleKing");
 
 // local development with docker compose
 Parse.serverURL = 'http://127.0.0.1:1337/parse';
-Parse.liveQueryServerURL = 'ws://127.0.0.1:1337/parse';
+Parse.liveQueryServerURL = 'ws://127.0.0.1:1338/parse';
 
 // DO hosted kubernetes
 // Parse.serverURL = 'https://parse-server.ryanapking.com/parse';
