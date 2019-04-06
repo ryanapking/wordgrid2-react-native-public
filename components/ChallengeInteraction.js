e7f46563fe6620ef3d86dde1d7d9966beca8f95b
Ryan Apking <ryan.apking@gmail.com>
2019-04-06 13:38:06 -0700

    Fixing minor display issue on challenge interaction component.

--- components/ChallengeInteraction.js
+++ components/ChallengeInteraction.js
@@ -75,6 +75,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    height: '100%',
   },
   twoColumns: {
     flexBasis: '50%',
